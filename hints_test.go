8f28f0fffa6f2adf0a87cb7262adef3feba192c1
Steve Manuel <nilslice@gmail.com>
2018-05-14 23:55:16 -0600

    support and test for multiple hints

--- hints_test.go
+++ hints_test.go
@@ -25,21 +25,35 @@ message PreviousRequest {}
 
 // @protolock:skip
 // @protolock:no-impl <- not a real hint, should pick up skip for ChannelChanger
+// @protolock:internal <- real internal hint, for testing
 service ChannelChanger {
   rpc Next(stream NextRequest) returns (Channel);
   rpc Previous(PreviousRequest) returns (stream Channel);
 }
+
+// @protolock:skip
+message Volume {
+	float32 level = 1;
+}
+
+service VolumeChanger {
+	rpc Increase(stream IncreaseRequest) returns (Volume);
+	rpc Decrease(DecreaseRequest) returns (Volume);
+  }
 `
 
 func TestHints(t *testing.T) {
+	SetDebug(true)
 	lock := parseTestProto(t, hintSkip)
 
 	for _, def := range lock.Definitions {
 		t.Run("skip:messages", func(t *testing.T) {
 			assert.Len(t, def.Def.Messages, 1)
+			assert.Equal(t, def.Def.Messages[0].Name, "NextRequest")
 		})
 		t.Run("skip:services", func(t *testing.T) {
-			assert.Len(t, def.Def.Services, 0)
+			assert.Len(t, def.Def.Services, 1)
+			assert.Equal(t, def.Def.Services[0].Name, "VolumeChanger")
 		})
 	}
 }
